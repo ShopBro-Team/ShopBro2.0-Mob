d20312de589b47db04e9cc25184f265025f62bdc
Steve Torrens <stevetorrens@Steves-MacBook-Air.local>
2018-06-20 21:39:46 +1200

    working on styling budget setting component

--- components/BudgetSetting.js
+++ components/BudgetSetting.js
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet, ScrollView,
 import { connect } from 'react-redux'
 import { addBudget } from '../redux/actions/budget'
 import { navigate } from '../redux/actions/navigate'
+import styles from '../styles/mainForms'
 
 class BudgetSetting extends Component {
   constructor(props){
@@ -25,77 +26,32 @@ class BudgetSetting extends Component {
   render() {
     return (
       <View style={styles.container}>
-         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-            <View>
-              <Text style={styles.getStartedText}>Set your budget $:</Text>
-              <TextInput 
-                placeholder='Your budget'
-                name='budget'
-                placeholderTextColor='#85756E'
-                returnKeyType='next'
-                // onSubmitEditing={() => this.passwordInput.focus()}
-                autoCorrect={false}
-                underlineColorAndroid='transparent'
-                style={styles.input}
-                onChangeText={(text) => this.setState({budget: text})}
-                />
-                <TouchableOpacity style={styles.buttonContainer} onPress={this.handleSubmit} >
-                    <Text style={styles.input}>SUBMIT</Text>
-                </TouchableOpacity>  
-            </View>
-          </ScrollView>
+
+        <Text style={styles.getStartedText}>Set your budget $:</Text>
+          <TextInput 
+            placeholder='Your budget'
+            name='budget'
+            placeholderTextColor='#85756E'
+            returnKeyType='next'
+            autoCorrect={false}
+            underlineColorAndroid='transparent'
+            style={styles.input}
+            onChangeText={(text) => this.setState({budget: text})}
+          />
+
+          <TouchableOpacity style={styles.buttonContainer} onPress={this.handleSubmit} >
+            <Text style={styles.buttonText}>SUBMIT</Text>
+          </TouchableOpacity> 
+
       </View> 
     )
   }
 }
 
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      paddingHorizontal: 10
-    },
-    contentContainer: {
-      paddingTop: 10,
-      paddingBottom: 10,
-    },
-    welcomeContainer: {
-      alignItems: 'center',
-      marginTop: 10,
-      marginBottom: 20,
-    },
-    welcomeImage: {
-      width: 100,
-      height: 80,
-      resizeMode: 'contain',
-      marginTop: 3,
-      marginLeft: -10,
-    },
-    getStartedContainer: {
-      alignItems: 'center',
-      marginHorizontal: 50,
-    },
-    codeHighlightContainer: {
-      backgroundColor: 'rgba(0,0,0,0.05)',
-      borderRadius: 3,
-      paddingHorizontal: 4,
-    },
-    getStartedText: {
-      fontSize: 17,
-      color: 'rgba(96,100,109, 1)',
-      lineHeight: 24,
-      textAlign: 'center',
-      
-    },
-    tabBarInfoText: {
-      fontSize: 17,
-      color: 'rgba(96,100,109, 1)',
-      textAlign: 'center',
-    },
-    navigationFilename: {
-      marginTop: 5,
-    },
-  });
+// const styles = StyleSheet.create ({
+//   container: {
+//     padding: 20
+//   }
+// })
 
 export default connect()(BudgetSetting)
