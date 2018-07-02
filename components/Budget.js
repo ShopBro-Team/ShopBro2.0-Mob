4c3f21ad0a1a93c191739998f51ac458e9c3cf14
Steve Torrens <stevetorrens@Steves-MacBook-Air.local>
2018-07-02 12:40:05 +1200

    Added some basic styling to budget components and validation

--- components/Budget.js
+++ components/Budget.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, ScrollView, 
-        TouchableOpacity, TextInput } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import { connect } from 'react-redux'
+import styles from '../styles/main'
 
 class Budget extends Component {
  
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <ScrollView >
           <View>
-            <Text style={styles.getStartedText}>Your budget is $: ${(this.props.budget/100).toFixed(2)}</Text>  
+            <Text style={styles.message}> Your budget is $: ${(this.props.budget/100).toFixed(2)} </Text>  
 
             { this.props.budget - this.props.totalSpend > 0
             ?
-            <Text >
+            <Text style={styles.message}>
             You have ${((this.props.budget - this.props.totalSpend)/100).toFixed(2)} left
             </Text>   
             :
-            <Text >
+            <Text style={styles.message}>
             You have over spent by ${((this.props.totalSpend - this.props.budget)/100).toFixed(2)}
             </Text> 
             }
@@ -30,61 +30,12 @@ class Budget extends Component {
   }
 }
 
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      paddingHorizontal: 10
-    },
-    contentContainer: {
-      paddingTop: 10,
-      paddingBottom: 10,
-    },
-    welcomeContainer: {
-      alignItems: 'center',
-      marginTop: 10,
-      marginBottom: 20,
-    },
-    welcomeImage: {
-      width: 100,
-      height: 80,
-      resizeMode: 'contain',
-      marginTop: 3,
-      marginLeft: -10,
-    },
-    getStartedContainer: {
-      alignItems: 'center',
-      marginHorizontal: 50,
-    },
-    codeHighlightContainer: {
-      backgroundColor: 'rgba(0,0,0,0.05)',
-      borderRadius: 3,
-      paddingHorizontal: 4,
-    },
-    getStartedText: {
-      fontSize: 17,
-      color: 'rgba(96,100,109, 1)',
-      lineHeight: 24,
-      textAlign: 'center',
-      
-    },
-    tabBarInfoText: {
-      fontSize: 17,
-      color: 'rgba(96,100,109, 1)',
-      textAlign: 'center',
-    },
-    navigationFilename: {
-      marginTop: 5,
-    },
-  });
-
-  const mapStateToProps = (state) => {
+const mapStateToProps = (state) => {
     
-    return {
-      budget: state.budget,
-      totalSpend: state.totalSpend
-    }
+  return {
+    budget: state.budget,
+    totalSpend: state.totalSpend
   }
+}
 
 export default connect(mapStateToProps)(Budget)
--- components/BudgetSetting.js
+++ components/BudgetSetting.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, ScrollView, 
-        TouchableOpacity, TextInput } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput } from 'react-native'
 import { connect } from 'react-redux'
 import { addBudget } from '../redux/actions/budget'
 import { navigate } from '../redux/actions/navigate'
-import styles from '../styles/mainForms'
+import styles from '../styles/main'
+import validateCostInput from '../utils/costInput'
 
 class BudgetSetting extends Component {
   constructor(props){
     super(props)
     this.state = {
       budget: '',
+      messageCost: '',
+      valid: true
     }
     this.handleSubmit = this.handleSubmit.bind(this)
   }
@@ -19,8 +21,17 @@ class BudgetSetting extends Component {
     event.preventDefault()
     let budget = this.state.budget*100
 
-    this.props.dispatch(addBudget(budget))
-    this.props.dispatch(navigate('budget'))
+    let checkValid =  validateCostInput(budget)
+	
+		this.setState({
+			messageCost : checkValid.messageCost,
+			valid : checkValid.valid
+		})
+
+    if(checkValid.valid) {
+      this.props.dispatch(addBudget(budget))
+      this.props.dispatch(navigate('budget'))
+    }
   }  
 
   render() {
@@ -28,30 +39,27 @@ class BudgetSetting extends Component {
       <View style={styles.container}>
 
         <Text style={styles.getStartedText}>Set your budget $:</Text>
-          <TextInput 
-            placeholder='Your budget'
-            name='budget'
-            placeholderTextColor='#85756E'
-            returnKeyType='next'
-            autoCorrect={false}
-            underlineColorAndroid='transparent'
-            style={styles.input}
-            onChangeText={(text) => this.setState({budget: text})}
-          />
 
-          <TouchableOpacity style={styles.buttonContainer} onPress={this.handleSubmit} >
-            <Text style={styles.buttonText}>SUBMIT</Text>
-          </TouchableOpacity> 
+        <TextInput 
+          placeholder='Your budget'
+          name='budget'
+          placeholderTextColor='#85756E'
+          returnKeyType='next'
+          autoCorrect={false}
+          underlineColorAndroid='transparent'
+          style={styles.input}
+          onChangeText={(text) => this.setState({budget: text})}
+        />
+
+        <TouchableOpacity style={styles.buttonContainer} onPress={this.handleSubmit} >
+          <Text style={styles.buttonText}>SUBMIT</Text>
+        </TouchableOpacity> 
+
+        {this.state.messageCost && <Text>{this.state.messageCost}</Text>}
 
       </View> 
     )
   }
 }
-
-// const styles = StyleSheet.create ({
-//   container: {
-//     padding: 20
-//   }
-// })
 
 export default connect()(BudgetSetting)
